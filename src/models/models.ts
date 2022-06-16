2228232da6ab725b14730362c26a8386b91b8766
maxkemzi <kemzi9585@gmail.com>
2022-06-16 13:32:41 +0300

    Add models folder - Add models.ts file
    Needed to export and use entity interfaces across the app.

---
+++ src/models/models.ts
@@ -0,0 +1,29 @@
+export interface ITask {
+	title: string;
+	tags: ITag[];
+	description: string;
+	creator: IUser;
+	id: string;
+	date: string;
+	isActive: boolean;
+}
+
+export interface IAchieve {
+	id: string;
+	description: string;
+}
+
+export interface IUser {
+	username: string;
+	avatar: string;
+}
+
+export interface ITag {
+	id: string;
+	text: string;
+}
+
+export interface IStat {
+	value: number;
+	title: string;
+}
