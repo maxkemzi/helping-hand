c9ee16214fd7a04146d9d619474e4407bb4df8ed
maxkemzi <kemzi9585@gmail.com>
2022-06-16 13:28:47 +0300

    Add utils folder - Add routes.ts file - Add variables.css file
    Needed for storing constant variables and helping utils and eventually to use them across the app.

---
+++ src/utils/constants/routes.ts
@@ -0,0 +1,8 @@
+export const HOME_ROUTE: string = "/";
+export const LOGIN_ROUTE: string = "/login";
+export const SIGNUP_ROUTE: string = "/signup";
+export const TASKS_ROUTE: string = "/tasks";
+export const PROFILE_ROUTE: string = "/profile";
+
+export const ROUTES_WITH_ABSOLUTE_HEADER = [HOME_ROUTE];
+export const ROUTES_WITH_BORDER_HEADER = [TASKS_ROUTE, PROFILE_ROUTE];
