008ce9e1427a0ad38dd5867e70af599f677dcd57
Max Kemzi <kemzi9585@gmail.com>
2022-06-19 21:39:30 +0300

    Add basic settings page layout

--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -6,4 +6,8 @@ export const PROFILE_ROUTE: string = "/profile";
 export const SETTINGS_ROUTE: string = "/settings";
 
 export const ROUTES_WITH_ABSOLUTE_HEADER = [HOME_ROUTE];
-export const ROUTES_WITH_BORDER_HEADER = [TASKS_ROUTE, PROFILE_ROUTE];
+export const ROUTES_WITH_BORDER_HEADER = [
+	TASKS_ROUTE,
+	PROFILE_ROUTE,
+	SETTINGS_ROUTE
+];
--- src/views/Settings/SettingsPage/SettingsPage.tsx
+++ src/views/Settings/SettingsPage/SettingsPage.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, {FC, useState} from "react";
+import classNames from "classnames";
+import AccountSettings from "@views/Settings/AccountSettings/AccountSettings";
+import TabList from "@components/TabList/TabList";
+import TabItem from "@components/TabItem/TabItem";
+import styles from "./SettingsPage.module.scss";
 
-const SettingsPage = () => <div>Settings</div>;
+const SettingsPage: FC = () => {
+	const [activeTab, setActiveTab] = useState(0);
+
+	return (
+		<div className={styles.page}>
+			<div className={classNames(styles.container, "container")}>
+				<h2 className={styles.title}>Налаштування</h2>
+				<div className={styles.inner}>
+					<TabList
+						className={styles.tabs}
+						variant="vertical"
+						activeTab={activeTab}
+					>
+						<TabItem
+							variant="vertical"
+							text="Акаунт"
+							onClick={() => setActiveTab(0)}
+							isActive={activeTab === 0}
+						/>
+						<TabItem
+							variant="vertical"
+							text="Інтеграція"
+							onClick={() => setActiveTab(1)}
+							isActive={activeTab === 1}
+						/>
+						<TabItem
+							variant="vertical"
+							text="Інтерфейс"
+							onClick={() => setActiveTab(2)}
+							isActive={activeTab === 2}
+						/>
+					</TabList>
+					<div className={styles.content}>
+						{activeTab === 0 && <AccountSettings />}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default SettingsPage;
