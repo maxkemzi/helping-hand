9d971b4235a40d5ea5215e31ef45c52f204a6e69
maxkemzi <kemzi9585@gmail.com>
2022-06-21 12:09:11 +0300

    Add components for images - Add fallback image component - Add status icon component

--- src/icons/FallbackImage/FallbackImage.tsx
+++ src/icons/FallbackImage/FallbackImage.tsx
@@ -1,17 +1,11 @@
 import React, {FC} from "react";
 import classNames from "classnames";
+import {IconProps} from "@customTypes/index";
 import styles from "./FallbackImage.module.scss";
 
 export type FallbackImageVariant = "light" | "lighter";
 
-interface FallbackImageProps {
-	className?: string;
-	variant?: FallbackImageVariant;
-	width?: number;
-	height?: number;
-}
-
-const FallbackImage: FC<FallbackImageProps> = ({
+const FallbackImage: FC<IconProps & {variant?: FallbackImageVariant}> = ({
 	className,
 	width,
 	height,
@@ -25,24 +19,15 @@ const FallbackImage: FC<FallbackImageProps> = ({
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
-		<path d="M32 0H0V32H32V0Z" fill="#252139" />
+		<path d="M32 0H0V32H32V0Z" fill="#2F2B44" />
 		<path
-			d="M15.617 17.3002H16.383C20.8268 17.3002 24.4366 20.8712 24.4992 25.3002H7.50083C7.56341 20.8712 11.1732 17.3002 15.617 17.3002ZM19.9999 10.5C19.9999 12.7091 18.209 14.5 15.9999 14.5C13.7908 14.5 11.9999 12.7091 11.9999 10.5C11.9999 8.29086 13.7908 6.5 15.9999 6.5C18.209 6.5 19.9999 8.29086 19.9999 10.5Z"
-			stroke="url(#paint0_linear_504_67)"
+			d="M8 24.6518C8 20.3255 11.4293 16.8183 15.6596 16.8183H16.3404C20.5707 16.8183 24 20.3255 24 24.6518C24 24.8441 23.8476 25 23.6596 25H8.34043C8.15241 25 8 24.8441 8 24.6518Z"
+			fill="#00D4C3"
 		/>
-		<defs>
-			<linearGradient
-				id="paint0_linear_504_67"
-				x1="16"
-				y1="6"
-				x2="16"
-				y2="25.8002"
-				gradientUnits="userSpaceOnUse"
-			>
-				<stop stopColor="#00D4C3" />
-				<stop offset="1" stopColor="#009AD2" />
-			</linearGradient>
-		</defs>
+		<path
+			d="M19.9999 11.0909C19.9999 13.3502 18.2091 15.1817 15.9999 15.1817C13.7908 15.1817 11.9999 13.3502 11.9999 11.0909C11.9999 8.83154 13.7908 7 15.9999 7C18.2091 7 19.9999 8.83154 19.9999 11.0909Z"
+			fill="#00D4C3"
+		/>
 	</svg>
 );
 
