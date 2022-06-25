49113746b0a984de4793306eefb3711cae95a830
maxkemzi <kemzi9585@gmail.com>
2022-06-25 17:32:38 +0300

    Change text field component prop types

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,23 +1,20 @@
-import React, {FC} from "react";
+import React, {FC, InputHTMLAttributes, TextareaHTMLAttributes} from "react";
 import classNames from "classnames";
+import {InputProps} from "@components/Input/Input";
 import styles from "./TextField.module.scss";
 
-interface TextFieldProps {
-	className?: string;
-	id: string;
+export interface TextFieldProps extends InputProps {
 	element: FC<{className?: string; id: string; required: boolean}>;
 	label: string;
-	isInvalid?: boolean;
-	required?: boolean;
 }
 
-const TextField: FC<TextFieldProps> = ({
-	className,
-	element: Element,
-	id,
-	label,
-	...props
-}) => (
+const TextField: FC<
+	TextFieldProps &
+		(
+			| InputHTMLAttributes<HTMLInputElement>
+			| TextareaHTMLAttributes<HTMLTextAreaElement>
+		)
+> = ({className, element: Element, id, label, ...props}) => (
 	<div className={classNames(className, styles.item)}>
 		<Element id={id} required className={styles.input} {...props} />
 		<label className={styles.label} htmlFor={id}>
