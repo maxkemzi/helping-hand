152f0e4e7e4212f37f2b6b34ec347256083e08f2
maxkemzi <kemzi9585@gmail.com>
2022-06-27 12:30:01 +0300

    Add className prop to logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,12 @@
 import React, {FC} from "react";
 import {NavLink} from "react-router-dom";
 import {HOME_ROUTE} from "@utils/constants/routes";
+import classNames from "classnames";
 import styles from "./Logo.module.scss";
 import LogoIcon from "../../icons/LogoIcon/LogoIcon";
 
-const Logo: FC = () => (
-	<NavLink className={styles.link} to={HOME_ROUTE}>
+const Logo: FC<{className?: string}> = ({className}) => (
+	<NavLink className={classNames(className, styles.link)} to={HOME_ROUTE}>
 		<LogoIcon />
 	</NavLink>
 );
