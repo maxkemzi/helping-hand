67ebca84ad2f22e188ece73b411ea10219249368
maxkemzi <kemzi9585@gmail.com>
2022-06-27 12:29:33 +0300

    Add language key to app state

--- src/store/app/app.slice.ts
+++ src/store/app/app.slice.ts
@@ -1,15 +1,17 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {AccentColor} from "@customTypes/index";
+import {AccentColor, Language} from "@customTypes/index";
 
 interface AppSliceState {
 	isInitializing: boolean;
 	accentColor: string;
+	language: Language;
 }
 
 // @ts-ignore
 const initialState: AppSliceState = {
 	isInitializing: false,
-	accentColor: localStorage.getItem("accentColor")
+	accentColor: localStorage.getItem("accentColor"),
+	language: "ua"
 };
 
 const appSlice = createSlice({
@@ -22,9 +24,13 @@ const appSlice = createSlice({
 		setAccentColor(state, action: PayloadAction<AccentColor>) {
 			state.accentColor = action.payload;
 			localStorage.setItem("accentColor", action.payload);
+		},
+		setLanguage(state, action: PayloadAction<Language>) {
+			state.language = action.payload;
 		}
 	}
 });
 
 export default appSlice.reducer;
-export const {setIsInitializing, setAccentColor} = appSlice.actions;
+export const {setIsInitializing, setAccentColor, setLanguage} =
+	appSlice.actions;
