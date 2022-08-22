d7a2d3120897ae0130faf25d71dd377ac80220b2
Max Kemzi <kemzi9585@gmail.com>
2022-08-22 17:57:53 +0300

    Add typed use selector hook

---
+++ src/hooks/useAppSelector.ts
@@ -0,0 +1,6 @@
+import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {RootState} from "@store/index";
+
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default useAppSelector;
