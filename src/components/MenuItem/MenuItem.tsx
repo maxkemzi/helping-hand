db9f4b15b3c4addac17c27942728620d7d323a44
Max Kemzi <kemzi9585@gmail.com>
2022-08-24 16:54:02 +0300

    Add some minor changes

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,13 +1,14 @@
 import React, {FC, MouseEventHandler} from "react";
 import classNames from "classnames";
 import {NavLink} from "react-router-dom";
+import {IconType} from "react-icons";
 import styles from "./MenuItem.module.scss";
 
 type As = "button" | "link";
 
 interface MenuItemProps {
 	className?: string;
-	icon: FC<{className?: string}>;
+	icon: IconType;
 	text: string;
 	path?: string;
 	onClick: MouseEventHandler;
@@ -33,7 +34,7 @@ const MenuItem: FC<MenuItemProps> = ({
 					[styles.active]: isActive
 				})}
 			>
-				<Icon className={styles.icon} />
+				<Icon className={styles.icon} size={24} />
 				<span>{text}</span>
 			</button>
 		);
@@ -48,7 +49,7 @@ const MenuItem: FC<MenuItemProps> = ({
 				classNames(className, styles.item, {[styles.active]: props.isActive})
 			}
 		>
-			<Icon className={styles.icon} />
+			<Icon className={styles.icon} size={24} />
 			<span>{text}</span>
 		</NavLink>
 	);
--- src/views/Profile/ProfileAchieves/ProfileAchieves.tsx
+++ src/views/Profile/ProfileAchieves/ProfileAchieves.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import AchieveItem from "@components/AchieveItem/AchieveItem";
 import classNames from "classnames";
-import VerifiedIcon from "@icons/VerifiedIcon/VerifiedIcon";
 import Achieve from "@customTypes/entities/achieve";
+import {IoRibbon} from "react-icons/io5";
 import styles from "./ProfileAchieves.module.scss";
 import data from "../../../mock.json";
 
 const ProfileAchieves = () => (
 	<div className={classNames("wrapper", styles.achieves)}>
 		{data.achieves.map((achieve: Achieve) => (
-			<AchieveItem key={achieve.id} {...achieve} icon={VerifiedIcon} />
+			<AchieveItem key={achieve.id} {...achieve} icon={IoRibbon} />
 		))}
 	</div>
 );
